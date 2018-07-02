820ffe45ffbd5d26fc6e2ecc634831c1280e2f53
Jamina Haeseli <haeselij@ethz.ch>
2018-07-02 21:22:12 +0200

    neuste version 21:22

--- Matlab_Git/one_segment_dynamic_model/dynamic_model_main.m
+++ Matlab_Git/one_segment_dynamic_model/dynamic_model_main.m
@@ -15,29 +15,44 @@ v_1_ = 0;
 v_2_ = 0;
 v_3_ = 0;
 
-q_1_ = 0.301401; 
+q_1_ = 0.3014; 
 q_2_ = 0.3014;
 q_3_ = 0.3014;
 pressure_total= zeros(12,10000);
-pressure_total(2,:) = 0.05;
+pressure_total(2,:) = 0.1;
+pressure_total(7,:) = 0.1;
+pressure_total(9,:) = 0.1;
+generalized_force_term_container = zeros(3, pressure_size);
+bending_stiffness_derivative_container = zeros(3, pressure_size);
+bending_stiffness_derivative_container = zeros(3, pressure_size);
+kinetic_energy_term_container = zeros(3, pressure_size);
+potential_energy_term_container = zeros(3, pressure_size);
+
+
+
 for i =  2:pressure_size %6967
- 
+ dt =(pressure_time(i)-pressure_time(i-1));
    q_container(1,i-1) = q_1_;
    q_container(2,i-1) = q_2_; 
-   q_container(3,i-1) = q_3_;
+   q_container(3,i-1) = q_3_;  
    
    v_container(1,i-1) = v_1_;
    v_container(2,i-1) = v_2_;
    v_container(3,i-1) = v_3_;
  
-   dt = pressure_time(i)-pressure_time(i-1);
   
-   [w_H_1h_, w_H_2dot_,dw_H_1h__dq1_, dw_H_1h__dq2_, dw_H_1h__dq3_, q_dot_, kappa_, l_bar_, phi_]= SetMatrices(dt, real(q_1_), real(q_2_), real(q_3_), real(v_1_), real(v_2_), real(v_3_));
-   [generalized_force_term_] = GetGeneralizedForceTerm(w_H_1h_, dw_H_1h__dq1_,dw_H_1h__dq2_, dw_H_1h__dq3_, real(q_1_), real(q_2_), real(q_3_), 10^5*pressure_total(2,i-1), 10^5*pressure_total(7,i-1), 10^5*pressure_total(9,i-1), v_1_,v_2_,v_3_);
-   [bending_stiffness_derivative_] = GetBendingStiffnessDeriv(10^5*pressure_total(2,i-1), 10^5*pressure_total(7,i-1), 10^5*pressure_total(9,i-1), kappa_*l_bar_, phi_,q_1_,q_2_,q_3_);
+   [w_H_1h_, w_H_2dot_,dw_H_1h__dq1_, dw_H_1h__dq2_, dw_H_1h__dq3_, q_dot_, kappa_, l_bar_, phi_]= SetMatrices(dt, q_1_, q_2_, q_3_, v_1_, v_2_, v_3_)
+   [generalized_force_term_] = GetGeneralizedForceTerm(w_H_1h_, dw_H_1h__dq1_,dw_H_1h__dq2_, dw_H_1h__dq3_, real(q_1_), real(q_2_), real(q_3_), 10^5*pressure_total(2,i-1), 10^5*pressure_total(7,i-1), 10^5*pressure_total(9,i-1), q_dot_)
+   [bending_stiffness_derivative_] = GetBendingStiffnessDeriv(10^5*pressure_total(2,i-1), 10^5*pressure_total(7,i-1), 10^5*pressure_total(9,i-1), kappa_*l_bar_, phi_,q_1_,q_2_,q_3_)
    [kinetic_energy_term_] = GetKineticEnergyTerm(dw_H_1h__dq1_, dw_H_1h__dq2_, dw_H_1h__dq3_, w_H_2dot_);
-   [potential_energy_term_] = GetPotentialEnergyTerm(real(q_1_), real(q_2_), real(q_3_), dw_H_1h__dq1_,dw_H_1h__dq2_, dw_H_1h__dq3_, kappa_, l_bar_, phi_, bending_stiffness_derivative_);
+   [potential_energy_term_] = GetPotentialEnergyTerm(real(q_1_), real(q_2_), real(q_3_), dw_H_1h__dq1_,dw_H_1h__dq2_, dw_H_1h__dq3_, kappa_, l_bar_, phi_, bending_stiffness_derivative_)
    
    [q_1_, q_2_, q_3_, v_1_, v_2_, v_3_] = LagrangeEquation(generalized_force_term_, kinetic_energy_term_, potential_energy_term_, q_dot_, real(v_1_), real(v_2_), real(v_3_))
+  
+   generalized_force_term_container(:,i-1) = generalized_force_term_; 
+   bending_stiffness_derivative_container(:,i) = bending_stiffness_derivative_;
+   [kinetic_energy_term_num] = GetNumKineticEnergy(q_1_, q_2_, q_3_, v_1_, v_2_, v_3_,kinetic_energy_term_);
+   kinetic_energy_term_container(:,i) = kinetic_energy_term_num;
+   potential_energy_term_container(:,i) = potential_energy_term_;
    
 end
--- Matlab_Git/one_segment_dynamic_model/symbolic_derrivation_d2w_H.m
+++ Matlab_Git/one_segment_dynamic_model/symbolic_derrivation_d2w_H.m
@@ -7,8 +7,9 @@ w_H_1h_2 = [sin(phi)*cos(phi)*(cos(theta) - 1), cos(phi)^2*(1 - cos(theta)) + co
 w_H_1h_3 = [-cos(phi)*sin(theta), -sin(phi)*sin(theta), cos(theta), sin(theta)/kappa - 0.3014];
 w_H_1h_4 = [0, 0, 0, 1];
 w_H_1h = [w_H_1h_1; w_H_1h_2; w_H_1h_3; w_H_1h_4];
+
 d_w_H_1h_dkappa = diff(w_H_1h,kappa);
-d_w_H_1h_dphi = diff(w_H_1h,phi);
+d_w_H_1h_dphi = diff(w_H_1h,phi)
 d_w_H_1h_dl_bar = diff(w_H_1h,l_bar);
 
 d_w_H_1h__dq1 =  diff(w_H_1h,q_1);
@@ -42,21 +43,26 @@ d2_w_H_1h_dkappadl_bar = diff(d_w_H_1h_dkappa, l_bar);
 % d_w_H_dk(13:16,3) = d_w_H_1h_dl_bar(:,4);
 %%
 % 
-% for i=1:3
-%     for k=1:3
-%         d_2_H_1h_dkdk_dd(i,k,:,:) = d2_w_H_1h_dphidphi*d_phi_dq(i)*d_phi_dq(k) + d2_w_H_1h_dphidkappa*d_phi_dq(i)*d_kappa_dq(k) + d2_w_H_1h_dphidl_bar*d_phi_dq(i)*d_l_bar_dq(k) + d2_w_H_1h_dphidkappa*d_kappa_dq(i)*d_phi_dq(k) + d2_w_H_1h_dkappadkappa*d_kappa_dq(i)*d_kappa_dq(k) + d2_w_H_1h_dkappadl_bar*d_kappa_dq(i)*d_l_bar_dq(k)   + d2_w_H_1h_dphidl_bar*d_l_bar_dq(i)*d_phi_dq(k) + d2_w_H_1h_dkappadl_bar*d_l_bar_dq(i)*d_kappa_dq(k) + d2_w_h_1h_dl_bard_lbar*d_l_bar_dq(i)*d_l_bar_dq(k);
-%         
-%         dH_1h_dk_dd(i,k,:,:) = d_w_H_1h_dphi*d2_phi_ddq(i,k) + d_w_H_1h_dkappa*d2_kappa_ddq(i,k) + d_w_H_1h_dl_bar*d2_l_bar_ddq(i,k);
-%         
-%         d2_H_1h_dq(i,k,:,:) = d_2_H_1h_dkdk_dd(i,k,:,:) + dH_1h_dk_dd(i,k,:,:);
-%     end
-% end
-% %%
-% for i=1:4   
-%     d2w_H_1h__dq1dq1(i,1) = d2_H_1h_dq(1,1,i,4);
-%     d2w_H_1h__dq1dq2(i,1) = d2_H_1h_dq(1,2,i,4);
-%     d2w_H_1h__dq1dq3(i,1) = d2_H_1h_dq(1,3,i,4);
-%     d2w_H_1h__dq2dq2(i,1) = d2_H_1h_dq(2,2,i,4);
-%     d2w_H_1h__dq2dq3(i,1) = d2_H_1h_dq(2,3,i,4);
-%     d2w_H_1h__dq3dq3(i,1) = d2_H_1h_dq(3,3,i,4);
-% end
+for i=1:3
+    for k=1:3
+        d_2_H_1h_dkdk_dd(i,k,:,:) = d2_w_H_1h_dphidphi*d_phi_dq(i)*d_phi_dq(k) + d2_w_H_1h_dphidkappa*d_phi_dq(i)*d_kappa_dq(k) + d2_w_H_1h_dphidl_bar*d_phi_dq(i)*d_l_bar_dq(k) + d2_w_H_1h_dphidkappa*d_kappa_dq(i)*d_phi_dq(k) + d2_w_H_1h_dkappadkappa*d_kappa_dq(i)*d_kappa_dq(k) + d2_w_H_1h_dkappadl_bar*d_kappa_dq(i)*d_l_bar_dq(k)   + d2_w_H_1h_dphidl_bar*d_l_bar_dq(i)*d_phi_dq(k) + d2_w_H_1h_dkappadl_bar*d_l_bar_dq(i)*d_kappa_dq(k) + d2_w_h_1h_dl_bard_lbar*d_l_bar_dq(i)*d_l_bar_dq(k);
+        
+        dH_1h_dk_dd(i,k,:,:) = d_w_H_1h_dphi*d2_phi_ddq(i,k) + d_w_H_1h_dkappa*d2_kappa_ddq(i,k) + d_w_H_1h_dl_bar*d2_l_bar_ddq(i,k);
+        
+        d2_H_1h_dq(i,k,:,:) = d_2_H_1h_dkdk_dd(i,k,:,:) + dH_1h_dk_dd(i,k,:,:);
+    end
+end
+%%
+for i=1:4   
+    d2w_H_1h__dq1dq1(i,1) = d2_H_1h_dq(1,1,i,4);
+    d2w_H_1h__dq1dq2(i,1) = d2_H_1h_dq(1,2,i,4);
+    d2w_H_1h__dq1dq3(i,1) = d2_H_1h_dq(1,3,i,4);
+    d2w_H_1h__dq2dq2(i,1) = d2_H_1h_dq(2,2,i,4);
+    d2w_H_1h__dq2dq3(i,1) = d2_H_1h_dq(2,3,i,4);
+    d2w_H_1h__dq3dq3(i,1) = d2_H_1h_dq(3,3,i,4);
+end
+%%
+if 4*(1-2*sin(phi)^2)*(1-cos(l_bar*kappa)) == d2_w_H_1h_dphidphi(1,1)
+    
+    abs=1
+end
