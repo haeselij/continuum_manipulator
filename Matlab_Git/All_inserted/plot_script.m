b85a2de6f8cc910b897a0feae24dca7954b987b2
Amir Hadzic <Amir@public-docking-lee-0948.ethz.ch>
2018-07-05 10:40:06 +0200

    changed folder structure

--- Matlab_Git/All_inserted/plot_script.m
+++
@@ -1,8 +0,0 @@
-figure; 
-plot(time, q_(1,1:numOfIterations), time, q_(2,1:numOfIterations), time, q_(3,1:numOfIterations))
-title('calculated muscle lengths p2 = 0.25')
-xlabel('Time [s]')
-ylabel('length [m]')
-legend('q1','q2','q3')
-axis([0 0.5 0 0.6])
-
