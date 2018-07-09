c5e03629c8a645e6482dd779a868c0a1f3c96e18
hadzica <hadzica@student.ethz.ch>
2018-07-09 16:08:44 +0200

    state before presentation

--- MatLAB/one_segment_dynamic_model/euler_forward/dynamic_model_main_euler_forward.m
+++ MatLAB/one_segment_dynamic_model/euler_forward/dynamic_model_main_euler_forward.m
@@ -2,20 +2,20 @@
 syms q_1_n q_2_n q_3_n v_1_n v_2_n v_3_n
 assume([q_1_n, q_2_n, q_3_n, v_1_n, v_2_n, v_3_n], 'real');
 %% Parameter list. !!After Changing parameters, run this section.!! 
-D_damp_spline_ = 0;
+D_damp_spline_ = 100;
 m_ = 0.265;
-g_ = - 9.81;
-C_ohne_l_bar_ = 2.64;
+g_ = -9.81;
+C_ohne_l_bar_ = -0.15;
 q_0_ = 0.3014;
 A_ = 0.0183*0.0183*pi;
-k_ = 1108.63;
-D_damp_ = 2000;
+k_ = 616.95;
+D_damp_ = 200;
 r_b_ = 0.026;
-k_spline_ = 100000;
+k_spline_ = 2.9822*10^4;
 
 %% main section
-numOfIterations = 10000;
-dt = 0.00001;
+numOfIterations = 20000;
+dt = 0.0005;
 
 q_ = zeros(3, numOfIterations);
 v_ = zeros(3, numOfIterations);
@@ -24,9 +24,31 @@ time = zeros(1, numOfIterations);
 
 q_(:,1) = [0.3014; 0.3014; 0.3014];
 v_(:,1) = [0; 0; 0];
-p_(:,1) = [0; 0.25; 0.25];
+for i=1:5000
+    p_(1,i) = 0;
+    p_(2,i) = 0.5*10^5/5000*i;
+    p_(3,i) = 0;
+end
+
+for i=5001:10000
+    p_(1,i) = 0;
+    p_(2,i) = 0.5*10^5*(1 - 1/5000*(i-5000));
+    p_(3,i) = 0; 
+end
+for i=10001:15000
+    p_(1,i) = 0;
+    p_(2,i) = 0.5*10^5/5000*(i-10000);
+    p_(3,i) = 0.5*10^5/5000*(i-10000);
+end
+
+for i=15001:20000
+    p_(1,i) = 0;
+    p_(2,i) = 0.5*10^5*(1 - 1/5000*(i-15000));
+    p_(3,i) = 0.5*10^5*(1 - 1/5000*(i-15000));
+end
 
 for i = 1:numOfIterations
+    tic;
     q_1_dot = 1/dt*(q_1_n - q_(1,i));
     q_2_dot = 1/dt*(q_2_n - q_(2,i));
     q_3_dot = 1/dt*(q_3_n - q_(3,i));
@@ -35,8 +57,9 @@ for i = 1:numOfIterations
     v_2_dot = 1/dt*(v_2_n - v_(2,i));
     v_3_dot = 1/dt*(v_3_n - v_(3,i));
     
-    [q_(:,i+1), v_(:,i+1)] = SolveSystem(D_damp_spline_, m_, g_, C_ohne_l_bar_, q_0_, A_, k_, D_damp_, r_b_, k_spline_, q_(1,i), q_(2,i), q_(3,i), v_(1,i), v_(2,i), v_(3,i), q_1_dot, q_2_dot, q_3_dot, v_1_dot, v_2_dot, v_3_dot, 0, 0, 0);
+    [q_(:,i+1), v_(:,i+1)] = SolveSystemEulerForward(dt ,D_damp_spline_, m_, g_, C_ohne_l_bar_, q_0_, A_, k_, D_damp_, r_b_, k_spline_, q_(1,i), q_(2,i), q_(3,i), v_(1,i), v_(2,i), v_(3,i), q_1_dot, q_2_dot, q_3_dot, v_1_dot, v_2_dot, v_3_dot, p_(1,i), p_(2,i), p_(3,i));
     time(1,i) = dt*i - dt;
+    toc
 end
 
 
--- MatLAB/one_segment_dynamic_model/plot_script.m
+++ MatLAB/one_segment_dynamic_model/plot_script.m
@@ -1,11 +1,21 @@
-figure; 
-plot(time, q_(1,1:numOfIterations), time, q_(2,1:numOfIterations), time, q_(3,1:numOfIterations))
-title('calculated muscle lengths p2 = 0.25')
-xlabel('Time [s]')
-ylabel('length [m]')
-legend('q1','q2','q3')
-axis([0 0.05 0 0.4])
 %%
 for i=1:numOfIterations
-   time(i) = dt*i - dt; 
+   time(i) = 2*dt*i - dt; 
+   l_bar_container(i) = (q_(1,i) + q_(2,i) + q_(3,i))/3;
+   
 end
+
+figure; 
+plot(time, q_(3,1:numOfIterations), time, q_(1,1:numOfIterations), time, q_(2,1:numOfIterations))
+title('calculated muscle lengths euler  ')
+xlabel('Time [s]')
+ylabel('Length [m]')
+legend('q_1','q_2','q_3')
+axis([0 20 0.25 0.35])
+hold on
+%plot(time, l_bar_container)
+%%
+figure;
+e_r = q_euler - q_;
+
+plot(time,e_r(1:20000))
