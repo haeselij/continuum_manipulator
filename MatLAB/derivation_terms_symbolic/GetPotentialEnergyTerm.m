2725668a732034e6016ed98382e360b6b4b2a896
hadzica <hadzica@student.ethz.ch>
2018-07-11 16:13:33 +0200

    derivation of symbolic terms code cleand and commented

--- MatLAB/derivation_terms_symbolic/GetPotentialEnergyTerm.m
+++ MatLAB/derivation_terms_symbolic/GetPotentialEnergyTerm.m
@@ -1,4 +1,5 @@
 function [potential_energy_term] = GetPotentialEnergyTerm(dw_H_1h__dq1, dw_H_1h__dq2, dw_H_1h__dq3, bending_stiffness_derivative)
+   
     syms m g C q_0 A k D_damp q_1_dot q_2_dot q_3_dot v_1_dot v_2_dot v_3_dot phi theta kappa r_b q_1 q_2 q_3 p_1 p_2 p_3 k_spline;
 
     w_g = [0; 0; -g; 0];     
@@ -12,4 +13,5 @@ function [potential_energy_term] = GetPotentialEnergyTerm(dw_H_1h__dq1, dw_H_1h_
     potential_energy_q3 = potential_energy_q3;
  
     potential_energy_term = [potential_energy_q1; potential_energy_q2; potential_energy_q3];
+    
 end
