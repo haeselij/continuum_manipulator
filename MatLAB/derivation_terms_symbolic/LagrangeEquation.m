e93715d08e0fdd1df26938e1b2e48dce0c2bb7e7
hadzica <hadzica@student.ethz.ch>
2018-07-17 16:42:00 +0200

    cleaned symbolic derivations

--- MatLAB/derivation_terms_symbolic/LagrangeEquation.m
+++ MatLAB/derivation_terms_symbolic/LagrangeEquation.m
@@ -1,10 +1,9 @@
 function [eqns] = LagrangeEquation(generalized_force_term, kinetic_energy_term, potential_energy_term)
-   
+
     eqn_1 = kinetic_energy_term(1) + potential_energy_term(1) == generalized_force_term(1);
     eqn_2 = kinetic_energy_term(2) + potential_energy_term(2) == generalized_force_term(2);
     eqn_3 = kinetic_energy_term(3) + potential_energy_term(3) == generalized_force_term(3);
 
     eqns = [eqn_1 eqn_2 eqn_3];
+    
 end
-
-
--- MatLAB/derivation_terms_symbolic/derive_eqn_main.m
+++ MatLAB/derivation_terms_symbolic/derive_eqn_main.m
@@ -1,17 +1,17 @@
-% Main script in order to derive Lagrange equations symbolically. Corresponding function 
-% files are SetMatrices, GetBendingStiffnessDeriv, GetGeneralizedForceTerm, 
-% GetKineticEnergyTerm and LagrangeEquation. 
+% Main script in order to derive Lagrange equations symbolically. Corresponding function
+% files are SetMatrices, GetBendingStiffnessDeriv, GetGeneralizedForceTerm,
+% GetKineticEnergyTerm and LagrangeEquation.
 
-%% 
+%%
 % Main section, where all terms of Lagrange equarions are collected and
-% saved. The result are the symbolic equations.
- 
-    % !!!!!! Equations are derived for two cases, one is the general case and the other 
+% saved. The results are the symbolic equations.
+
+    % !!!!!! Equations are derived for two cases, one is the general case and the other
     % one is the case where singularity occurs. Depednig on for which case
-    % the equations has to be derived 'calculate_singularity_case' is set
+    % the equations have to be derived 'calculate_singularity_case' is set
     % HERE to true for singularity case and false for general one. !!!!!!
     calculate_singularity_case = true;
-    
+
     [w_H_h_, w_H_2dot_, dw_H_h__dq1_, dw_H_h__dq2_, dw_H_h__dq3_]= SetMatrices(calculate_singularity_case);
     [bending_stiffness_derivative_] = GetBendingStiffnessDeriv();
 
@@ -20,14 +20,14 @@
     [potential_energy_term_] = GetPotentialEnergyTerm(dw_H_h__dq1_, dw_H_h__dq2_, dw_H_h__dq3_, bending_stiffness_derivative_);
 
     [eqns_] = LagrangeEquation(generalized_force_term_, kinetic_energy_term_, potential_energy_term_);
-   
-%% 
+
+%%
 % Print section, where equations are printed in text files. Depending one which one we
 % are calculating, the filename (defined under variable 'strX') has to
 % be adapted.
 
     if(calculate_singularity_case)
-        
+
         str1 = sprintf('eqn1_singularity');
         eqn1_file = fopen(str1, 'wt');
         fprintf(eqn1_file, '%s\n', char(eqns_(1)));
@@ -42,9 +42,9 @@
         eqn3_file = fopen(str3, 'wt');
         fprintf(eqn3_file, '%s\n', char(eqns_(3)));
         fclose(eqn3_file);
-        
+
     else
-        
+
         str1 = sprintf('eqn1');
         eqn1_file = fopen(str1, 'wt');
         fprintf(eqn1_file, '%s\n', char(eqns_(1)));
@@ -59,5 +59,5 @@
         eqn3_file = fopen(str3, 'wt');
         fprintf(eqn3_file, '%s\n', char(eqns_(3)));
         fclose(eqn3_file);
-        
+
     end
